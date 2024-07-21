43c2a3d0e041ac099003bdf307892f24967706c7
Patrick Lizoń <12571855+patricklizon@users.noreply.github.com>
2024-07-22 00:17:21 +0200

    uncomments install scripts

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 set -e
 
-# ./bin/run.sh install ssh
+./bin/run.sh install ssh
 
-# ./bin/run.sh install brew
-# ./bin/run.sh install zsh
+./bin/run.sh install brew
+./bin/run.sh install zsh
 ./bin/run.sh install omz
 
-# ./bin/run.sh install node
-# ./bin/run.sh install git
-# ./bin/run.sh install github
-# ./bin/run.sh install zed
+./bin/run.sh install node
+./bin/run.sh install git
+./bin/run.sh install github
+./bin/run.sh install zed
