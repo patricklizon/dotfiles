4996a23fea7e94ea82afb3cc9b87faf096a5eb71
Patrick Lizoń <12571855+patricklizon@users.noreply.github.com>
2024-07-31 10:34:35 +0200

    tweak tmux installation process

--- tmux/install.sh
+++ tmux/install.sh
@@ -4,24 +4,32 @@ set -e
 source "${PWD}/constants.sh"
 
 DIR="${CONFIG_DIR}/tmux/"
+TPM_DIR="${HOME}/.tmux/plugin/tpm"
 PWD_PATH="${PWD}/tmux/"
 
-main () {
+install_tpm() {
+    if test ! -d ~/.tmux/plugins/tpm "${TPM_DIR}"; then
+       git clone "https://github.com/tmux-plugins/tpm ${TPM_DIR}"
+    fi
+
+    ${TPM_DIR}/bin/install_plugins
+}
+
+main() {
     echo "Setting up tmux...\n"
 
     if [ ! -d "${DIR}" ]; then
         mkdir -p ${DIR}
     fi
-    if [ -d "${HOME}/.tmux/plugins/tpm" ]; then
-        rm -rf "${HOME}/.tmux/plugins/tpm"
-    fi
-
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
     echo "\nLinking files...\n"
 
     local file="tmux.conf"
     ln -sf "${PWD_PATH}${file}" "${DIR}${file}"
-    }
+
+    echo "\nInstalling tpm...\n"
+
+    install_tpm
+}
 
 main
