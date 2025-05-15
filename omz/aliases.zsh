dcb7c6f3f7efacf95b0178b0390ce6ade47927b4
Patrick Lizoń <12571855+patricklizon@users.noreply.github.com>
2025-05-15 11:18:54 +0200

    removes aliases containing corepack

--- omz/aliases.zsh
+++ omz/aliases.zsh
@@ -2,14 +2,6 @@
 alias zshconfig="${EDITOR} ${HOME}/.zshrc"
 alias ohmyzsh="${EDITOR} ${HOME}/.oh-my-zsh"
 
-# corepack
-alias yarn="corepack yarn"
-alias yarnpkg="corepack yarnpkg"
-alias pnpm="corepack pnpm"
-alias pnpx="corepack pnpx"
-alias npm="corepack npm"
-alias npx="corepack npx"
-
 # omz
 alias reload="omz reload"
 
