8c65f3ceaafef13f47c0888e4362c053e530d4e0
Patrick Lizoń <12571855+patricklizon@users.noreply.github.com>
2026-07-21 14:39:59 +0200

    fix: ssh name

--- constants.sh
+++ constants.sh
@@ -1,5 +1,5 @@
 SSH_KEY_TYPE="ed25519"
-SSH_KEY_PATH="${HOME}/.ssh/id_${KEY_TYPE}"
+SSH_KEY_PATH="${HOME}/.ssh/id_${SSH_KEY_TYPE}"
 SSH_CONFIG_DIR="${HOME}/.ssh/config"
 
 CONFIG_DIR="${HOME}/.config"
