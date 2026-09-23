f812c3c38a0b872c9de47be173eb140338175a9f
Patryk Lizoń <12571855+patricklizon@users.noreply.github.com>
2026-09-23 12:50:12 +0200

    fix: target listening process in killport

--- omz/functions.zsh
+++ omz/functions.zsh
@@ -1,3 +1,10 @@
 killport() {
-	-ti :$1 | xargs kill -9;
+	if [[ $# -ne 1 || "$1" != <-> || "$1" -lt 1 || "$1" -gt 65535 ]]; then
+		print -u2 'Usage: killport <port>'
+		return 2
+	fi
+
+	local pids
+	pids=$(lsof -nP -tiTCP:"$1" -sTCP:LISTEN) || return 1
+	kill -TERM -- ${(f)pids}
 }
